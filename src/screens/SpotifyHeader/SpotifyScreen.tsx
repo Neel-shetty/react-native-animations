7587d2a25230a7ec109f62a3b1846928733fdc2c
Neel <neelnarayanshetty@gmail.com>
2022-12-04 12:04:09 +0530

    setup

--- src/screens/SpotifyHeader/SpotifyScreen.tsx
+++ src/screens/SpotifyHeader/SpotifyScreen.tsx
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Asset } from "expo-asset";
+import { StatusBar } from "react-native";
+import AppLoading from "expo-app-loading";
+
+import Album from "./components/Album";
+import { Album as AlbumModel } from "./components/Model";
+
+const album: AlbumModel = {
+  name: "Remote Control",
+  artist: "Jan Blomqvist",
+  release: 2016,
+  // eslint-disable-next-line global-require
+  cover: require("./assets/Jan-Blomqvist.jpg"),
+  tracks: [
+    { name: "Stories Over" },
+    { name: "More", artist: "Jan Blomqvist, Elena Pitoulis" },
+    { name: "Empty Floor" },
+    { name: "Her Great Escape" },
+    { name: "Dark Noise" },
+    { name: "Drift", artist: "Jan Blomqvist, Aparde" },
+    { name: "Same Mistake" },
+    {
+      name: "Dancing People Are Never Wrong",
+      artist: "Jan Blomqvist, The Bianca Story",
+    },
+    { name: "Back in the Taxi" },
+    { name: "Ghosttrack" },
+    { name: "Just OK" },
+    { name: "The End" },
+  ],
+};
 
 const SpotifyScreen = () => {
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    (async () => {
+      await Asset.loadAsync(album.cover);
+      setReady(true);
+    })();
+  });
+  if (!ready) {
+    return <AppLoading />;
+  }
   return (
-    <View>
-      <Text>SpotifyScreen</Text>
-    </View>
-  )
-}
+    <>
+      <StatusBar barStyle="light-content" />
+      <Album {...{ album }} />
+    </>
+  );
+};
 
-export default SpotifyScreen
-
-const styles = StyleSheet.create({})
+export default SpotifyScreen;
---
+++ src/screens/SpotifyHeader/components/Content.tsx
@@ -0,0 +1,124 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, {
+  event,
+  Value,
+  interpolate,
+  Extrapolate,
+  useScrollViewOffset
+} from "react-native-reanimated";
+// import { onScroll } from "react-native-redash";
+
+import { Album, MAX_HEADER_HEIGHT, MIN_HEADER_HEIGHT } from "./Model";
+import Track from "./Track";
+import ShufflePlay, { BUTTON_HEIGHT } from "./ShufflePlay";
+import Header from "./Header";
+
+interface ContentProps {
+  album: Album;
+  y: Animated.Value<number>;
+}
+
+console.log();
+// const { Interpolate, Extrapolate } = Animated;
+
+const x = new Value(0);
+const contentOffset = { x };
+const onScroll = {
+  x,
+  scrollHandler: {
+    onScroll: event([
+      {
+        nativeEvent: {
+          contentOffset,
+        },
+      },
+    ]),
+    scrollEventThrottle: 1,
+  },
+};
+
+export default ({ album: { artist, tracks }, y }: ContentProps) => {
+  const height = interpolate(
+    0.5,
+    [-MAX_HEADER_HEIGHT, -BUTTON_HEIGHT / 2],
+    [0, MAX_HEADER_HEIGHT + BUTTON_HEIGHT],
+    Extrapolate.CLAMP
+  );
+  const opacity = interpolate(
+    0.5,
+    [-MAX_HEADER_HEIGHT / 2, 0, MAX_HEADER_HEIGHT / 2],
+    [0, 1, 0],
+    Extrapolate.CLAMP
+  );
+  return (
+    <Animated.ScrollView
+      // onScroll={onScroll({ y })}
+      style={styles.container}
+      showsVerticalScrollIndicator={false}
+      scrollEventThrottle={1}
+      stickyHeaderIndices={[1]}
+    >
+      <View style={styles.cover}>
+        <Animated.View style={[styles.gradient, { height }]}>
+          <LinearGradient
+            style={StyleSheet.absoluteFill}
+            start={[0, 0.3]}
+            end={[0, 1]}
+            colors={["transparent", "rgba(0, 0, 0, 0.2)", "black"]}
+          />
+        </Animated.View>
+        <View style={styles.artistContainer}>
+          <Animated.Text style={[styles.artist, { opacity }]}>
+            {artist}
+          </Animated.Text>
+        </View>
+      </View>
+      <View style={styles.header}>
+        <Header {...{ y, artist }} />
+        <ShufflePlay />
+      </View>
+      <View style={styles.tracks}>
+        {tracks.map((track, key) => (
+          <Track index={key + 1} {...{ track, key, artist }} />
+        ))}
+      </View>
+    </Animated.ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: MIN_HEADER_HEIGHT - BUTTON_HEIGHT / 2,
+  },
+  cover: {
+    height: MAX_HEADER_HEIGHT - BUTTON_HEIGHT,
+  },
+  gradient: {
+    position: "absolute",
+    left: 0,
+    bottom: 0,
+    right: 0,
+    alignItems: "center",
+  },
+  artistContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  artist: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 48,
+    fontWeight: "bold",
+  },
+  header: {
+    marginTop: -BUTTON_HEIGHT,
+  },
+  tracks: {
+    paddingTop: 32,
+    backgroundColor: "black",
+  },
+});
